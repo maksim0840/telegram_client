4dee8befd6e8fbb98c367cdf52d501514697b7bc
maksim0840 <maksim0840@gmail.com>
2025-03-21 09:29:01 +0300

    encrypt / decrypt beta

--- Telegram/lib_extension/extension/encryption/message_text_encryption.cpp
+++ Telegram/lib_extension/extension/encryption/message_text_encryption.cpp
@@ -7,7 +7,11 @@ QString encrypt_the_message(const QString& text, const quint64 chat_id, const qu
     std::string my_id_str = std::to_string(my_id);
     std::string new_text_str = text_str;
 
-    
+    KeysDataBase db;
+    std::optional<std::string> session_key = db.get_active_param_text(chat_id_str, KeysTablesDefs::AES, AesColumnsDefs::SESSION_KEY);
+
+    AesKeyManager aes_manager;
+
     int rsa_key_len_default = 2048;
     bool dh_fastmode_default = true;
     std::regex re_options(R"(\[/start_encryption-(\d+)-([01])\])");
@@ -23,6 +27,11 @@ QString encrypt_the_message(const QString& text, const quint64 chat_id, const qu
     if (text_str == "[/start_encryption]") { // начать формирование общего ключа
         ChatCommandsManager commands;
         new_text_str = commands.start_rsa(chat_id_str, my_id_str, rsa_key_len_default, dh_fastmode_default);
+    }
+    else if (session_key) {
+        Message message;
+        message.text = aes_manager.encrypt_message(new_text_str, *session_key);
+        new_text_str = message.get_text_with_options();
     }
 
     std::cout << '\n';
